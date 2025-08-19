43bb0b960bcf24adff502abbd61aa4349d4791dd
hobbyden <hobbydenofficial@gmail.com>
2025-08-19 16:04:58 +0530

    frontend url changed

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,12 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: process.env.frontend_URL || "https://jjobtracker.netlify.app/",
+    credentials: true,
+  })
+);
 
 app.use("/user", userRoutes);
 
