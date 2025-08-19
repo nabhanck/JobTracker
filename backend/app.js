ff42b8690a74969bee3fab54c6da9db807e075d9
hobbyden <hobbydenofficial@gmail.com>
2025-08-19 16:25:47 +0530

    frontend url fix

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.frontend_URL || "https://jjobtracker.netlify.app/",
+    origin: process.env.frontend_URL || "https://jjobtracker.netlify.app",
     credentials: true,
   })
 );
