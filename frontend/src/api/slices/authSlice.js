c741c771330d80c22fe450f3e7d8ed624632024a
hobbyden <hobbydenofficial@gmail.com>
2025-08-19 17:01:28 +0530

    frontend api credential fix

--- frontend/src/api/slices/authSlice.js
+++ frontend/src/api/slices/authSlice.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 
 export const loginSlice = (data) => {
-  return axios.post(`${import.meta.env.VITE_BACKEND_URL}/user/login`, data);
+  return axios.post(`${import.meta.env.VITE_BACKEND_URL}/user/login`, data, {
+    withCredentials: true,
+  });
 };
 
 export const registerSlice = (registerData) => {
   return axios.post(
     `${import.meta.env.VITE_BACKEND_URL}/user/signup`,
-    registerData
+    registerData,
+    { withCredentials: true }
   );
 };
